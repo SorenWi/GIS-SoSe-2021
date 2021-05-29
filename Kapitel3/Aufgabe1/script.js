95c57b2e309031e0ef2dbfe092a8e6fb4b3e6da0
Soeren <soerenwinterhalder@gmail.com>
2021-05-29 20:48:00 +0200

    3.1

--- Kapitel3/Aufgabe1/script.js
+++ Kapitel3/Aufgabe1/script.js
@@ -1,9 +1,20 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.a3_1 = void 0;
-const Http = require("http");
 var a3_1;
 (function (a3_1) {
-    Http.createServer();
-})(a3_1 = exports.a3_1 || (exports.a3_1 = {}));
+    let formData;
+    let url = "https://gissose2021soren.herokuapp.com/";
+    let submitBtn = document.getElementById("submitBtn");
+    submitBtn.addEventListener("click", click);
+    async function click() {
+        formData = new FormData(document.forms[0]);
+        let query = new URLSearchParams(formData);
+        url += "?" + query.toString();
+        let response = await fetch(url, { method: "get" });
+        showResponse(await response.text());
+    }
+    function showResponse(response) {
+        let responseDiv = document.getElementById("resposeDiv");
+        responseDiv.innerHTML = response;
+    }
+})(a3_1 || (a3_1 = {}));
 //# sourceMappingURL=script.js.map
--- Kapitel3/Aufgabe1/script.ts
+++ Kapitel3/Aufgabe1/script.ts
@@ -1,7 +1,21 @@
-import * as Http from "http";
-import * as Url from "url";
+namespace a3_1 {
+    let formData: FormData;
+    let url: string = "https://gissose2021soren.herokuapp.com/";
 
-export namespace a3_1 {
-    Http.createServer();
+    let submitBtn: HTMLButtonElement = <HTMLButtonElement> document.getElementById("submitBtn");
+    submitBtn.addEventListener("click", click);
+
+    async function click(): Promise<void> {
+        formData = new FormData(document.forms[0]);
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url += "?" + query.toString();
+        let response: Response = await fetch(url, { method: "get"});
+        showResponse(await response.text());
+    }
+
+    function showResponse(response: string): void {
+        let responseDiv: HTMLDivElement = <HTMLDivElement> document.getElementById("resposeDiv");
+        responseDiv.innerHTML = response;
+    }
+
 }
-
