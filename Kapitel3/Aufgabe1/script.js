ca1577cda59fdf8cb9e7bd9dd717332f9de169cb
Soeren <soerenwinterhalder@gmail.com>
2021-05-29 18:50:28 +0200

    server setup

---
+++ Kapitel3/Aufgabe1/script.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.a3_1 = void 0;
+const Http = require("http");
+var a3_1;
+(function (a3_1) {
+    Http.createServer();
+})(a3_1 = exports.a3_1 || (exports.a3_1 = {}));
+//# sourceMappingURL=script.js.map
---
+++ Kapitel3/Aufgabe1/script.ts
@@ -0,0 +1,7 @@
+import * as Http from "http";
+import * as Url from "url";
+
+export namespace a3_1 {
+    Http.createServer();
+}
+
